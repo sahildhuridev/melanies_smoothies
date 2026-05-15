90c500f849dbe7fbb62002f2eea755533ac2f63f
Sahil Sadanand Dhuri <sahildhuri.dev216@gmail.com>
2026-05-15 15:46:29 +0530

    Integrate fruit nutrition API in Streamlit app
    Added requests import and API call for fruit nutrition.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import requests  
 #from snowflake.snowpark.context import get_active_session
 # Write directly to the app.
 cnx = st.connection("snowflake")
@@ -35,6 +36,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_Container_width=True)
+
     
     #st.write(ingredients_string)
 
@@ -52,9 +57,7 @@ if ingredients_list:
 
 
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+
 
 
 
